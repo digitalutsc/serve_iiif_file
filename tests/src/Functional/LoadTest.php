42be72e9bec19c3d12dab0c38eb56f39992d98c6
Aryan-R-Patel <aryanp.031206@gmail.com>
2026-04-16 11:48:05 -0400

    feat: add functional LoadTest.php

---
+++ tests/src/Functional/LoadTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Drupal\Tests\serve_iiif_file\Functional;
+
+use Drupal\Core\Url;
+use Drupal\Tests\BrowserTestBase;
+use PHPUnit\Framework\Attributes\RunTestsInSeparateProcesses;
+
+/**
+ * Simple test to ensure that main page loads with module enabled.
+ *
+ * @group serve_iiif_file
+ */
+#[RunTestsInSeparateProcesses]
+class LoadTest extends BrowserTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  protected static $modules = ['serve_iiif_file'];
+
+  /**
+   * A user with permissions required to access the configuration form.
+   *
+   * @var \Drupal\user\UserInterface
+   */
+  protected $user;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+    $this->user = $this->drupalCreateUser([
+      'access administration pages',
+      'administer site configuration',
+    ]);
+    $this->drupalLogin($this->user);
+  }
+
+  /**
+   * Tests that the home page loads with a 200 response.
+   */
+  public function testLoad(): void {
+    $this->drupalGet('<front>');
+    $this->assertSession()->statusCodeEquals(200);
+  }
+
+}
